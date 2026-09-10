AlphaDroid-Project/packages_apps_Settings

Settings: add blur algorithm preference
Show a blur algorithm selector in Display settings when SurfaceControl
reports supported algorithms and a valid persisted token exists.

Populate entries from SurfaceControl, persist the selected token to
persist.sys.renderengine.blur_algorithm, and prompt for a system
restart after changes.

src/com/android/settings/DisplaySettings.java
@@ -19,9 +19,15 @@ package com.android.settings;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.view.SurfaceControl;
+
+import com.android.internal.util.alpha.SystemRestartUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
 
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.BrightnessLevelPreferenceController;
@@ -47,9 +53,15 @@ import java.util.List;
 public class DisplaySettings extends DashboardFragment {
     private static final String TAG = "DisplaySettings";
 
+    private static final String KEY_BLUR_ALGORITHM = "blur_algorithm";
+    private static final String PROP_PERSIST_RENDERENGINE_BLUR_ALGORITHM =
+            "persist.sys.renderengine.blur_algorithm";
     private static final String KEY_HIGH_TOUCH_POLLING_RATE = "high_touch_polling_rate_enable";
     private static final String KEY_HIGH_TOUCH_SENSITIVITY = "high_touch_sensitivity_enable";
     private static final String KEY_PROXIMITY_ON_WAKE = "proximity_on_wake";
+
+    @Nullable
+    private ListPreference mBlurAlgorithmPref;
 
     @Override
     public int getMetricsCategory() {
@@ -69,6 +81,14 @@ public class DisplaySettings extends DashboardFragment {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        mBlurAlgorithmPref = findPreference(KEY_BLUR_ALGORITHM);
+        refreshBlurAlgorithmPreference();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshBlurAlgorithmPreference();
     }
 
     @Override
@@ -95,6 +115,54 @@ public class DisplaySettings extends DashboardFragment {
         return controllers;
     }
 
+    /**
+     * Show blur algorithm list only when native has written {@code persist.sys.renderengine
+     * .blur_algorithm}. If empty or unknown, hide (native seeds default when both props unset).
+     */
+    private void refreshBlurAlgorithmPreference() {
+        if (mBlurAlgorithmPref == null) {
+            return;
+        }
+
+        final SurfaceControl.SupportedBlurAlgorithms algorithms =
+                SurfaceControl.getSupportedBlurAlgorithms();
+        if (algorithms == null || algorithms.propertyTokens.length == 0) {
+            mBlurAlgorithmPref.setVisible(false);
+            return;
+        }
+
+        final String persisted =
+                SystemProperties.get(PROP_PERSIST_RENDERENGINE_BLUR_ALGORITHM, "");
+        if (TextUtils.isEmpty(persisted)) {
+            mBlurAlgorithmPref.setVisible(false);
+            return;
+        }
+
+        String selected = null;
+        for (String token : algorithms.propertyTokens) {
+            if (TextUtils.equals(token, persisted)) {
+                selected = persisted;
+                break;
+            }
+        }
+        if (selected == null) {
+            mBlurAlgorithmPref.setVisible(false);
+            return;
+        }
+
+        mBlurAlgorithmPref.setEntries(algorithms.enumLabels);
+        mBlurAlgorithmPref.setEntryValues(algorithms.propertyTokens);
+        mBlurAlgorithmPref.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
+        mBlurAlgorithmPref.setValue(selected);
+        mBlurAlgorithmPref.setVisible(true);
+        mBlurAlgorithmPref.setOnPreferenceChangeListener((preference, newValue) -> {
+            SystemProperties.set(
+                    PROP_PERSIST_RENDERENGINE_BLUR_ALGORITHM, String.valueOf(newValue));
+            SystemRestartUtils.showSystemRestartDialog(requireContext());
+            return true;
+        });
+    }
+
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(R.xml.display_settings) {
 
@@ -113,6 +181,15 @@ public class DisplaySettings extends DashboardFragment {
                     if (!context.getResources().getBoolean(
                             com.android.internal.R.bool.config_proximityCheckOnWake)) {
                         keys.add(KEY_PROXIMITY_ON_WAKE);
+                    }
+                    final SurfaceControl.SupportedBlurAlgorithms blurAlgorithms =
+                            SurfaceControl.getSupportedBlurAlgorithms();
+                    if (blurAlgorithms == null
+                            || blurAlgorithms.propertyTokens.length == 0
+                            || TextUtils.isEmpty(
+                                    SystemProperties.get(
+                                            PROP_PERSIST_RENDERENGINE_BLUR_ALGORITHM, ""))) {
+                        keys.add(KEY_BLUR_ALGORITHM);
                     }
                     return keys;
                 }

Inventor365/packages_apps_Settings-lunaris

Signed-off-by: Ghosuto <clash.raja10@gmail.com>

src/com/android/settings/DisplaySettings.java
@@ -19,9 +19,15 @@ package com.android.settings;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.view.SurfaceControl;
+
+import com.android.internal.util.android.SystemRestartUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
 
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.BrightnessLevelPreferenceController;
@@ -47,9 +53,15 @@ import java.util.List;
 public class DisplaySettings extends DashboardFragment {
     private static final String TAG = "DisplaySettings";
 
+    private static final String KEY_BLUR_ALGORITHM = "blur_algorithm";
+    private static final String PROP_PERSIST_RENDERENGINE_BLUR_ALGORITHM =
+            "persist.sys.renderengine.blur_algorithm";
     private static final String KEY_HIGH_TOUCH_POLLING_RATE = "high_touch_polling_rate_enable";
     private static final String KEY_HIGH_TOUCH_SENSITIVITY = "high_touch_sensitivity_enable";
     private static final String KEY_PROXIMITY_ON_WAKE = "proximity_on_wake";
+
+    @Nullable
+    private ListPreference mBlurAlgorithmPref;
 
     @Override
     public int getMetricsCategory() {
@@ -69,6 +81,14 @@ public class DisplaySettings extends DashboardFragment {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        mBlurAlgorithmPref = findPreference(KEY_BLUR_ALGORITHM);
+        refreshBlurAlgorithmPreference();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshBlurAlgorithmPreference();
     }
 
     @Override
@@ -95,6 +115,54 @@ public class DisplaySettings extends DashboardFragment {
         return controllers;
     }
 
+    /**
+     * Show blur algorithm list only when native has written {@code persist.sys.renderengine
+     * .blur_algorithm}. If empty or unknown, hide (native seeds default when both props unset).
+     */
+    private void refreshBlurAlgorithmPreference() {
+        if (mBlurAlgorithmPref == null) {
+            return;
+        }
+
+        final SurfaceControl.SupportedBlurAlgorithms algorithms =
+                SurfaceControl.getSupportedBlurAlgorithms();
+        if (algorithms == null || algorithms.propertyTokens.length == 0) {
+            mBlurAlgorithmPref.setVisible(false);
+            return;
+        }
+
+        final String persisted =
+                SystemProperties.get(PROP_PERSIST_RENDERENGINE_BLUR_ALGORITHM, "");
+        if (TextUtils.isEmpty(persisted)) {
+            mBlurAlgorithmPref.setVisible(false);
+            return;
+        }
+
+        String selected = null;
+        for (String token : algorithms.propertyTokens) {
+            if (TextUtils.equals(token, persisted)) {
+                selected = persisted;
+                break;
+            }
+        }
+        if (selected == null) {
+            mBlurAlgorithmPref.setVisible(false);
+            return;
+        }
+
+        mBlurAlgorithmPref.setEntries(algorithms.enumLabels);
+        mBlurAlgorithmPref.setEntryValues(algorithms.propertyTokens);
+        mBlurAlgorithmPref.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
+        mBlurAlgorithmPref.setValue(selected);
+        mBlurAlgorithmPref.setVisible(true);
+        mBlurAlgorithmPref.setOnPreferenceChangeListener((preference, newValue) -> {
+            SystemProperties.set(
+                    PROP_PERSIST_RENDERENGINE_BLUR_ALGORITHM, String.valueOf(newValue));
+            SystemRestartUtils.showSystemRestartDialog(requireContext());
+            return true;
+        });
+    }
+
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(R.xml.display_settings) {
 
@@ -113,6 +181,15 @@ public class DisplaySettings extends DashboardFragment {
                     if (!context.getResources().getBoolean(
                             org.lineageos.platform.internal.R.bool.config_proximityCheckOnWake)) {
                         keys.add(KEY_PROXIMITY_ON_WAKE);
+                    }
+                    final SurfaceControl.SupportedBlurAlgorithms blurAlgorithms =
+                            SurfaceControl.getSupportedBlurAlgorithms();
+                    if (blurAlgorithms == null
+                            || blurAlgorithms.propertyTokens.length == 0
+                            || TextUtils.isEmpty(
+                                    SystemProperties.get(
+                                            PROP_PERSIST_RENDERENGINE_BLUR_ALGORITHM, ""))) {
+                        keys.add(KEY_BLUR_ALGORITHM);
                     }
                     return keys;
                 }